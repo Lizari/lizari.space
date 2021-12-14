adf18e3e3e0ef8649962649b7b019253b7cd452c
Lizari <muroya725@gmail.com>
2021-12-14 22:28:29 +0900

    AboutMe作った

---
+++ src/components/About/AboutCard.tsx
@@ -0,0 +1,37 @@
+import React, {ReactNode} from "react";
+import {Box, Center, Text} from "@chakra-ui/react";
+import FadeIn from "@/components/Common/FadeIn";
+
+type Props = {
+    column: string,
+    children: ReactNode
+}
+
+const AboutCard: React.VFC<Props> = (props) => {
+    return(
+        <FadeIn>
+            <Center>
+                <Box w={"80%"}
+                     mt={"5vh"}
+                     p={"3px"}
+                     boxShadow={"2xl"}
+                     borderRadius={"xl"}
+                     textAlign={"center"}
+                     textColor={"black"}
+                     bg={"white"}>
+                    <Text fontWeight={"semibold"}
+                          fontSize={{base: "20px", md: "30px"}}
+                          mb={"8px"}>
+                        {props.column}
+                    </Text>
+                    <Box fontWeight={"hairline"}
+                         fontSize={{base: "16px", md: "22px"}}>
+                        {props.children}
+                    </Box>
+                </Box>
+            </Center>
+        </FadeIn>
+    )
+}
+
+export default AboutCard;
