570ce06e1a26ac65f2cf6ba7fc2145a23e02a978
Lizari <muroya725@gmail.com>
2021-12-16 00:00:28 +0900

    esbuild-loaderの導入

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
+  webpack: (config, { webpack, isServer }) => {
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+          React: 'react',
+        }),
+    );
+
+    useEsbuildMinify(config);
+    useEsbuildLoader(config, {
+      loader: 'jsx',
+      target: 'es2017',
+    });
+
+
     if (!isServer) {
       config.resolve.fallback.fs = false;
       config.resolve.fallback.child_process = false;
@@ -10,4 +25,23 @@ module.exports = {
     }
     return config;
   },
+}
+
+function useEsbuildMinify(config, options) {
+  const { minimizer } = config.optimization;
+  const terserIndex = minimizer.findIndex(
+      minifier => minifier.constructor.name === 'TerserPlugin',
+  );
+
+  minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+}
+
+function useEsbuildLoader(config, options) {
+  const { rules } = config.module;
+  const rule = rules.find(rule => rule.test.test('.js'));
+
+  rule.use = {
+    loader: 'esbuild-loader',
+    options,
+  };
 }
