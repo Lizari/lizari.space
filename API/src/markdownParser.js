2f47af170c45359306fdece8f486e3316065bae7
Lizari <muroya725@gmail.com>
2022-02-01 20:08:07 +0900

    Markdownの書式と被っているためbracket変更

--- API/src/markdownParser.js
+++ API/src/markdownParser.js
@@ -2,14 +2,14 @@
     @param { String } content - markdownの中身
 */
 exports.parse = function (content) {
-    const bracket = "---";
+    const bracket = "===";
     const body = content.split(/\r?\n/);
     const headerMd = body.splice(1, body.lastIndexOf(bracket) - 1);
     const encoded = base64Encode(content.substring(content.lastIndexOf(bracket) + bracket.length));
     let header = {};
 
     for (const line of headerMd) {
-        const obj = line.replace(/\s/g, '').replace(/.'/g, '').split(":");
+        const obj = line.replace(/\s/g, '').replace(/.'/g, '').split(/:(.+)/);
 
         header[obj[0]] = obj[1];
     }
